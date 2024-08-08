0297b956f7ac7c45bd6fe4c19ddc3880795b3205
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:28:05 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Get started!
+let name=prompt("what's your name")
+console.log(name)
+
+let weather=prompt(`hi, ${name} guess the weather for today`)
+let guess=prompt("no, try again")
+let idk=prompt("no, please try again")
+let ik=prompt("would you like to know the answer")
+let id=prompt("whats your cip code")
+parseInt("17.5")
+console.log(`okay, ${id}, the weather is 8 ${weather}`)
+
+  
